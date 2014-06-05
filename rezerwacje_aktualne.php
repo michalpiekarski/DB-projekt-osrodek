56f78ee736ad07eb2e379a0abac819744a17fed9
Michał Piekarski <michal.piekarski@icloud.com>
2014-06-05 22:57:52 +0200

    Wyświetlanie rezerwacji. Zmiana układu tabel.

--- rezerwacje_aktualne.php
+++ rezerwacje_aktualne.php
@@ -14,21 +14,42 @@
 		oci_execute($rezerwacja);
 	?>
 
-	<div class='basic-grey'>
+	<div class='basic-grey' style="width: 60%;">
 		<h1>Aktualne rezerwacje</h1>
 
-		<table class='basic-grey' style='border: none; padding: 0; text-align: center;' cellpadding='5em'>
+		<table class='basic-grey' style='border: none; padding: 0; text-align: center; width: 100%;' cellpadding='5em'>
 
 		<?php
 			while($row = oci_fetch_array($rezerwacja)) {
 				echo "<tr>";
-				echo "<td rowspan='2' style='font-size: 1.5em;'>".$row['ID']."</td>";
-				echo "<td>ID Rachunku: ".$row['RACHUNKI_ID']."</td>";
-				echo "<td>ID Obiektu: ".$row['OBIEKTY_ID']."</td>";
+				echo "<th rowspan='3' style='font-size: 1.5em;'>".$row['ID']."</th>";
+				echo "<th colspan='2'>Klient</th>";
+				echo "<th colspan='2'>Obiekt</th>";
 				echo "</tr>";
+
+				$id_rachunku = $row['RACHUNKI_ID'];
+				$sql_id_klienta = oci_parse($con, "SELECT * FROM RACHUNKI WHERE ID = '$id_rachunku'");
+				oci_execute($sql_id_klienta);
+				$id_klienta_array = oci_fetch_array($sql_id_klienta);
+
+				$id_klienta = $id_klienta_array['KLIENCI_ID'];
+				$sql_klient = oci_parse($con, "SELECT * FROM KLIENCI WHERE ID = '$id_klienta'");
+				oci_execute($sql_klient);
+				$klient_array = oci_fetch_array($sql_klient);
+
+				$id_obiektu = $row['OBIEKTY_ID'];
+
+				echo"<tr>";
+				echo"<td>".$klient_array['IMIE']."</td>";
+				echo"<td>".$klient_array['NAZWISKO']."</td>";
+				echo"<td colspan='2'>".$id_obiektu."</td>";
+				echo"</tr>";
+
 				echo "<tr>";
-				echo "<td style='border-bottom: solid 1px lightgrey;'>Data od: ".$row['DATA_OD']."</td>";
-				echo "<td style='border-bottom: solid 1px lightgrey; border-right: solid 1px lightgrey;'>Data do: ".$row['DATA_DO']."</td>";
+				echo "<th style='border-bottom: solid 1px lightgrey;'>Data od:</th>";
+				echo "<td style='border-bottom: solid 1px lightgrey;'>".$row['DATA_OD']."</td>";
+				echo "<th style='border-bottom: solid 1px lightgrey;'>Data do:</th>";
+				echo "<td style='border-bottom: solid 1px lightgrey; border-right: solid 1px lightgrey;'>".$row['DATA_DO']."</td>";
 				echo "</tr>";
 			}
 			oci_close($con);
