5d0caca77c284c5d472826aa27d0ac4d609ed1d8
Michał Piekarski <michal.piekarski@icloud.com>
2014-06-08 00:52:33 +0200

    osrodek.php "posprzątany" (sporo usunięte, kilka rzeczy zmienione - działa nadal tak samo)

--- osrodek.php
+++ osrodek.php
@@ -5,16 +5,18 @@
 	<link rel="stylesheet" type="text/css" href="css/menu.css" />
 	<link rel="stylesheet" type="text/css" href="css/form.css" />
 	<link rel="stylesheet" type="text/css" href="css/progres.css" />
-<script type="text/javascript">
-	function showStuff(id) {
-		document.getElementById(id).style.display = 'block';
-	}
-	function hideStuff(id) {
-		document.getElementById(id).style.display = 'none';
-	}
-</script>
+	<script type="text/javascript">
+		function showStuff() {
+			document.getElementById('klient').style.display = 'block';
+			document.getElementById('form').action = 'add_exist.php';
+		}
+		function hideStuff() {
+			document.getElementById('klient').style.display = 'none';
+			document.getElementById('form').action = 'add.php';
+		}
+	</script>
 </head>
-<body onload="hideStuff('answer1');">
+<body onload="hideStuff();">
 
 	<?php
 		include('nav.php');
@@ -22,15 +24,11 @@
 		$con = oci_connect("tomek", "2") or die ("could not connect to oracledb");
 		$osrodek = oci_parse($con,"Select * from OSRODKI");
 		oci_execute($osrodek);
-		$osrodek2 = oci_parse($con,"Select * from OSRODKI");
-		oci_execute($osrodek2);
 		$klient = oci_parse($con, "Select ID, IMIE, NAZWISKO from KLIENCI");
 		oci_execute($klient);
-
 	?>
 
-	<form action='add.php' method='post' class='basic-grey'>
-
+	<form id='form' action='add.php' method='post' class='basic-grey'>
 
 		<h1>Wybierz ośrodek lub istniejącego klienta</h1>
 
@@ -47,62 +45,38 @@
 				</div>
 			</div>
 		</h2>
-		Czy klient znajduje się w bazie?
 
+		<h3>Czy klient znajduje się w bazie?
+			<span style="float: right; margin-right: 2em;">
+				<input type="radio" onclick="showStuff();" name="radio1" value="tak" />Tak
+				<input type="radio" onclick="hideStuff();" name="radio1" value="nie" checked/>Nie
+			</span>
+		</h3>
 
-		<input type="radio" onclick="showStuff('answer1'); hideStuff('przycisk1'); showStuff('przycisk2'); hideStuff('s1');" name="radio1" value="tak" />Tak
-		<input type="radio" onclick="hideStuff('answer1'); showStuff('przycisk1'); hideStuff('przycisk2'); showStuff('s1');" name="radio1" value="nie" checked/>Nie
-
-		<label id="s1">
+		<label>
 			<span>Ośrodek :</span>
-			<select name='selection' >
+			<select name='osrodek' >
 
 				<?php
 					while($row = oci_fetch_array($osrodek))
-						echo"<option value='".$row['ID']."'>".$row['NAZWA']."</option>";
+						echo"<option value='".$row['NAZWA']."'>".$row['NAZWA']."</option>";
 				?>
 
 			</select>
 		</label>
-
-
-
-
-		<label id="przycisk1">
-			<span>&nbsp;</span>
-			<input type="SUBMIT" class="button" value="Dalej" />
-		</label>
-	</form>
-
-	<form action="add_exist.php" method="post" class="basic-grey" id="answer1">
-	<h1>Wybierz Klienta</h1>
-
-
-	<label>
-			<span>Ośrodek :</span>
-			<select name='selection' id="s2">
-
-				<?php
-					while($row2 = oci_fetch_array($osrodek2))
-						echo"<option value='".$row2['ID']."'>".$row2['NAZWA']."</option>";
-				?>
-
-			</select>
-		</label>
-
-
-	<label>
+		<label id='klient'>
 			<span >Klient: </span>
 			<select name='klient'>
+
 				<?php
 					while($row = oci_fetch_array($klient))
 						echo"<option value='".$row['ID']."'>".$row['IMIE']." ".$row['NAZWISKO']."</option>";
 
 				?>
+
 			</select>
 		</label>
-
-			<label id="przycisk2">
+		<label>
 			<span>&nbsp;</span>
 			<input type="SUBMIT" class="button" value="Dalej" />
 		</label>
