1d6b80c6275afba45281031b5e2d2c0efe826b21
Michał Piekarski <michal.piekarski@icloud.com>
2014-06-09 01:34:43 +0200

    Adaptacja przestrzeni na stronach głównych części głównej i panelu administracyjnego. Dodany prototyp siatki odnośników, który należy jeszcze wypełnić odpowiednio przygotowanymi grafikami.

--- admin/index.php
+++ admin/index.php
@@ -1,13 +1,146 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" type="text/css" href="../css/menu.css" />
-	<link rel="stylesheet" type="text/css" href="../css/form.css" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" type="text/css" href="../css/menu.css" />
+    <link rel="stylesheet" type="text/css" href="../css/form.css" />
+    <style type="text/css">
+        .imagelink {
+            border: solid 1px black;
+            width: 120px;
+            height: 100px;
+            text-align: center;
+            float: left;
+            padding-top: 20px;
+            margin-left: 20px;
+            margin-bottom: 10px;
+        }
+        .clear {
+            clear: both;
+        }
+    </style>
 </head>
 <body>
-	<?php
-		include('nav.php');
-	?>
+
+    <?php
+        include('nav.php');
+    ?>
+
+    <div class="basic-grey" style="margin-top: 100px;">
+            <a href="osrodki.php">
+            <div class="imagelink">
+                TUTAJ LINK OBRAZKOWY
+                <hr width="50%" />
+                OŚRODKI
+            </div>
+        </a>
+        <a href="obiekty.php">
+            <div class="imagelink">
+                TUTAJ LINK OBRAZKOWY
+                <hr width="50%" />
+                OBIEKTY
+            </div>
+        </a>
+        <a href="pracownicy.php">
+            <div class="imagelink">
+                TUTAJ LINK OBRAZKOWY
+                <hr width="50%" />
+                PRACOWNICY
+            </div>
+        </a>
+        <a href="typy_zamowien.php">
+            <div class="imagelink">
+                TUTAJ LINK OBRAZKOWY
+                <hr width="50%" />
+                TYPY ZAMÓWIEŃ
+            </div>
+        </a>
+        <div class="clear"></div>
+        <a href="osrodek.php">
+            <div class="imagelink">
+                TUTAJ LINK OBRAZKOWY
+                <hr width="50%" />
+                DODAJ OŚRODEK
+            </div>
+        </a>
+        <div class="imagelink">
+            <br/>
+            <span style="font-size: 3em;">
+                LOGO
+            </span>
+        </div>
+        <a href="stanowisko.php">
+            <div class="imagelink">
+                TUTAJ LINK OBRAZKOWY
+                <hr width="50%" />
+                DODAJ STANOWISKO
+            </div>
+        </a>
+        <a href="pracownik.php">
+            <div class="imagelink">
+                TUTAJ LINK OBRAZKOWY
+                <hr width="50%" />
+                DODAJ PRACOWNIKA
+            </div>
+        </a>
+        <div class="clear"></div>
+        <a href="typ_obiektu.php">
+            <div class="imagelink">
+                TUTAJ LINK OBRAZKOWY
+                <hr width="50%" />
+                DODAJ TYP OBIEKTU
+            </div>
+        </a>
+        <a href="domek.php">
+            <div class="imagelink">
+                TUTAJ LINK OBRAZKOWY
+                <hr width="50%" />
+                DODAJ DOMEK
+            </div>
+        </a>
+        <a href="pokoj.php">
+            <div class="imagelink">
+                TUTAJ LINK OBRAZKOWY
+                <hr width="50%" />
+                DODAJ POKÓJ
+            </div>
+        </a>
+        <a href="inny_obiekt.php">
+            <div class="imagelink">
+                TUTAJ LINK OBRAZKOWY
+                <hr width="50%" />
+                DODAJ INNY OBIEKT
+            </div>
+        </a>
+        <div class="clear"></div>
+        <a href="posilek.php">
+            <div class="imagelink">
+                TUTAJ LINK OBRAZKOWY
+                <hr width="50%" />
+                DODAJ TYP POSIŁKU
+            </div>
+        </a>
+        <a href="usluga.php">
+            <div class="imagelink">
+                TUTAJ LINK OBRAZKOWY
+                <hr width="50%" />
+                DODAJ TYP USŁUGI
+            </div>
+        </a>
+        <a href="wypozyczenie.php">
+            <div class="imagelink">
+                TUTAJ LINK OBRAZKOWY
+                <hr width="50%" />
+                DODAJ TYP WYPOŻYCZENIA
+            </div>
+        </a>
+        <div class="imagelink">
+            <br/>
+            <span style="line-height: 2em;">
+                TM - AUTOR MESSAGE
+            </span>
+        </div>
+        <div class="clear"></div>
+    </div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -4,10 +4,84 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<link rel="stylesheet" type="text/css" href="css/menu.css" />
 	<link rel="stylesheet" type="text/css" href="css/form.css" />
+    <style type="text/css">
+        .imagelink {
+            border: solid 1px black;
+            width: 150px;
+            height: 110px;
+            text-align: center;
+            float: left;
+            padding-top: 40px;
+            margin-left: 20px;
+            margin-bottom: 10px;
+        }
+        .clear {
+            clear: both;
+        }
+    </style>
 </head>
 <body>
+
 	<?php
 		include('nav.php');
 	?>
+
+    <div class="basic-grey" style="margin-top: 100px;">
+            <a href="rezerwacje_aktualne.php">
+            <div class="imagelink">
+                TUTAJ LINK OBRAZKOWY
+                <hr width="50%" />
+                REZERWACJE AKTUALNE
+            </div>
+        </a>
+        <a href="zamowienia_aktualne.php">
+            <div class="imagelink">
+                TUTAJ LINK OBRAZKOWY
+                <hr width="50%" />
+                ZAMOWIENIA AKTUALNE
+            </div>
+        </a>
+        <a href="rachunki_otwarte.php">
+            <div class="imagelink">
+                TUTAJ LINK OBRAZKOWY
+                <hr width="50%" />
+                RACHUNKI OTWARTE
+            </div>
+        </a>
+        <div class="clear"></div>
+        <a href="rezerwacja.php">
+            <div class="imagelink">
+                TUTAJ LINK OBRAZKOWY
+                <hr width="50%" />
+                NOWA REZERWACJA
+            </div>
+        </a>
+        <div class="imagelink" style="width: 322px;">
+            <span style="font-size: 5em;">LOGO</span>
+        </div>
+        <div class="clear"></div>
+        <a href="posilek.php">
+            <div class="imagelink">
+                TUTAJ LINK OBRAZKOWY
+                <hr width="50%" />
+                ZAMOW POSILEK
+            </div>
+        </a>
+        <a href="uslugi.php">
+            <div class="imagelink">
+                TUTAJ LINK OBRAZKOWY
+                <hr width="50%" />
+                ZAMÓW USŁUGĘ
+            </div>
+        </a>
+        <a href="wypozyczenie.php">
+            <div class="imagelink">
+                TUTAJ LINK OBRAZKOWY
+                <hr width="50%" />
+                WYPOŻYCZ
+            </div>
+        </a>
+        <div class="clear"></div>
+    </div>
 </body>
 </html>
