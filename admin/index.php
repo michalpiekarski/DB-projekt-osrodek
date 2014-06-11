aa7bc262f99cdb8761d9ab0766cc7349b87a62de
Michał Piekarski <michal.piekarski@icloud.com>
2014-06-12 01:52:58 +0200

    Użycie nowych grafik na głównych stronach części głównej i administracyjnej. Implementacja podmiany grafik. Brakuje jeszcze tylko odpowiednich grafik dla części administracyjnej.

--- admin/index.php
+++ admin/index.php
@@ -12,52 +12,52 @@
         include('nav.php');
     ?>
 
-    <div class="basic-grey" style="margin-top: 80px; width: 720px; text-align: center;">
+    <div class="basic-grey admin">
         <a href="osrodki.php">
-            <img src="images/osrodki.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
+            <img src="images/osrodki.PNG" alt="Aktualne Rezerwacje" class="imagelink" onmouseover="this.src='images/osrodki.PNG'" onmouseout="this.src='images/osrodki.PNG'" />
         </a>
         <a href="klienci.php">
-            <img src="images/klienci.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
+            <img src="images/klienci.PNG" alt="Aktualne Rezerwacje" class="imagelink" onmouseover="this.src='images/klienci.PNG'" onmouseout="this.src='images/klienci.PNG'" />
         </a>
         <a href="pracownicy.php">
-            <img src="images/pracownicy.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
+            <img src="images/pracownicy.PNG" alt="Aktualne Rezerwacje" class="imagelink" onmouseover="this.src='images/pracownicy.PNG'" onmouseout="this.src='images/pracownicy.PNG'" />
         </a>
         <a href="obiekty.php">
-            <img src="images/obiekty.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
+            <img src="images/obiekty.PNG" alt="Aktualne Rezerwacje" class="imagelink" onmouseover="this.src='images/obiekty.PNG'" onmouseout="this.src='images/obiekty.PNG'" />
         </a>
         <a href="osrodek.php">
-            <img src="images/dodaj_osrodek.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
+            <img src="images/dodaj_osrodek.PNG" alt="Aktualne Rezerwacje" class="imagelink" onmouseover="this.src='images/dodaj_osrodek.PNG'" onmouseout="this.src='images/dodaj_osrodek.PNG'" />
         </a>
         <a href="stanowisko.php">
-            <img src="images/dodaj_stanowisko.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
+            <img src="images/dodaj_stanowisko.PNG" alt="Aktualne Rezerwacje" class="imagelink" onmouseover="this.src='images/dodaj_stanowisko.PNG'" onmouseout="this.src='images/dodaj_stanowisko.PNG'" />
         </a>
         <a href="pracownik.php">
-            <img src="images/dodaj_pracownika.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
+            <img src="images/dodaj_pracownika.PNG" alt="Aktualne Rezerwacje" class="imagelink" onmouseover="this.src='images/dodaj_pracownika.PNG'" onmouseout="this.src='images/dodaj_pracownika.PNG'" />
         </a>
         <img src="images/tm.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
         <a href="typ_obiektu.php">
-            <img src="images/dodaj_typ_obiektu.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
+            <img src="images/dodaj_typ_obiektu.PNG" alt="Aktualne Rezerwacje" class="imagelink" onmouseover="this.src='images/dodaj_typ_obiektu.PNG'" onmouseout="this.src='images/dodaj_typ_obiektu.PNG'" />
         </a>
         <a href="domek.php">
-            <img src="images/dodaj_domek.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
+            <img src="images/dodaj_domek.PNG" alt="Aktualne Rezerwacje" class="imagelink" onmouseover="this.src='images/dodaj_domek.PNG'" onmouseout="this.src='images/dodaj_domek.PNG'" />
         </a>
         <a href="pokoj.php">
-            <img src="images/dodaj_pokoj.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
+            <img src="images/dodaj_pokoj.PNG" alt="Aktualne Rezerwacje" class="imagelink" onmouseover="this.src='images/dodaj_pokoj.PNG'" onmouseout="this.src='images/dodaj_pokoj.PNG'" />
         </a>
         <a href="inny_obiekt.php">
-            <img src="images/dodaj_inny_obiekt.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
+            <img src="images/dodaj_inny_obiekt.PNG" alt="Aktualne Rezerwacje" class="imagelink" onmouseover="this.src='images/dodaj_inny_obiekt.PNG'" onmouseout="this.src='images/dodaj_inny_obiekt.PNG'" />
         </a>
         <a href="typy_zamowien.php">
-            <img src="images/typy_zamowien.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
+            <img src="images/typy_zamowien.PNG" alt="Aktualne Rezerwacje" class="imagelink" onmouseover="this.src='images/typy_zamowien.PNG'" onmouseout="this.src='images/typy_zamowien.PNG'" />
         </a>
         <a href="posilek.php">
-            <img src="images/dodaj_typ_posilku.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
+            <img src="images/dodaj_typ_posilku.PNG" alt="Aktualne Rezerwacje" class="imagelink" onmouseover="this.src='images/dodaj_typ_posilku.PNG'" onmouseout="this.src='images/dodaj_typ_posilku.PNG'" />
         </a>
         <a href="usluga.php">
-            <img src="images/dodaj_typ_uslugi.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
+            <img src="images/dodaj_typ_uslugi.PNG" alt="Aktualne Rezerwacje" class="imagelink" onmouseover="this.src='images/dodaj_typ_uslugi.PNG'" onmouseout="this.src='images/dodaj_typ_uslugi.PNG'" />
         </a>
         <a href="wypozyczenie.php">
-            <img src="images/dodaj_typ_wypozyczenia.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
+            <img src="images/dodaj_typ_wypozyczenia.PNG" alt="Aktualne Rezerwacje" class="imagelink" onmouseover="this.src='images/dodaj_typ_wypozyczenia.PNG'" onmouseout="this.src='images/dodaj_typ_wypozyczenia.PNG'" />
         </a>
     </div>
 </body>
--- index.php
+++ index.php
@@ -12,28 +12,28 @@
 		include('nav.php');
 	?>
 
-    <div class="basic-grey" style="margin-top: 100px; text-align: center;">
+    <div class="basic-grey">
         <a href="rezerwacje_aktualne.php">
-            <img src="images/aktualne_rezerwacje.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
+            <img src="images/aktualne_rezerwacje.PNG" alt="Aktualne Rezerwacje" class="imagelink" onmouseover="this.src='images/aktualne_rezerwacje_hov.PNG'" onmouseout="this.src='images/aktualne_rezerwacje.PNG'"/>
         </a>
         <a href="zamowienia_aktualne.php">
-            <img src="images/aktualne_zamowienia.PNG" alt="Aktualne Zamówienia" class="imagelink" />
+            <img src="images/aktualne_zamowienia.PNG" alt="Aktualne Zamówienia" class="imagelink" onmouseover="this.src='images/aktualne_zamowienia_hov.PNG'" onmouseout="this.src='images/aktualne_zamowienia.PNG'" />
         </a>
         <a href="rachunki_otwarte.php">
-            <img src="images/rachunki_otwarte.PNG" alt="Otwarte Rachunki" class="imagelink" />
+            <img src="images/rachunki_otwarte.PNG" alt="Otwarte Rachunki" class="imagelink" onmouseover="this.src='images/rachunki_otwarte_hov.PNG'" onmouseout="this.src='images/rachunki_otwarte.PNG'" />
         </a>
         <a href="rezerwacja.php">
-            <img src="images/nowa_rezerwacja.PNG" alt="Zarezerwuj" class="imagelink" />
+            <img src="images/nowa_rezerwacja.PNG" alt="Zarezerwuj" class="imagelink" onmouseover="this.src='images/nowa_rezerwacja_hov.PNG'" onmouseout="this.src='images/nowa_rezerwacja.PNG'" />
         </a>
         <img id="logo" src="images/logo.PNG" alt="Logo" />
         <a href="posilek.php">
-            <img src="images/zamow_posilek.PNG" alt="Zamów Posiłek" class="imagelink" />
+            <img src="images/zamow_posilek.PNG" alt="Zamów Posiłek" class="imagelink" onmouseover="this.src='images/zamow_posilek_hov.PNG'" onmouseout="this.src='images/zamow_posilek.PNG'" />
         </a>
         <a href="uslugi.php">
-            <img src="images/zamow_usluge.PNG" alt="Zamów Usługę" class="imagelink" />
+            <img src="images/zamow_usluge.PNG" alt="Zamów Usługę" class="imagelink" onmouseover="this.src='images/zamow_usluge_hov.PNG'" onmouseout="this.src='images/zamow_usluge.PNG'" />
         </a>
         <a href="wypozyczenie.php">
-            <img src="images/wypozycz.PNG" alt="Wypożycz" class="imagelink" />
+            <img src="images/wypozycz.PNG" alt="Wypożycz" class="imagelink" onmouseover="this.src='images/wypozycz_hov.PNG'" onmouseout="this.src='images/wypozycz.PNG'" />
         </a>
     </div>
 </body>
