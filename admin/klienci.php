ad37b6d02d3bd809f16e10cefd00b5a40e4032eb
Michał Piekarski <michal.piekarski@icloud.com>
2014-06-11 14:43:44 +0200

    Początki implementacji nowej funkcjonalności na stronach wyświetlających tabele zawartości bazy (edycja i usuwanie). Dodatkowo na potrzeby tejże funkcjonalności zostaje wprowadzona nieco zmieniona struktura tych tabel (thead oraz tbody).

--- admin/klienci.php
+++ admin/klienci.php
@@ -16,38 +16,46 @@
 	?>
 
 	<div class='basic-grey'>
-		<h1>Ośrodki</h1>
+		<h1>Klienci</h1>
 
 		<table class='basic-grey' style='border: none; padding: 0; text-align: center;' cellpadding='5em'>
-			<tr>
-				<th rowspan='2' style='background-color: lightgrey; font-size: 1.4em;'>ID</th>
-				<th style='background-color: lightgrey;'>Imię</th>
-				<th style='background-color: lightgrey;'>Nazwisko</th>
-				<th style='background-color: lightgrey;'>Telefon</th>
-				<th style='background-color: lightgrey;'>E-mail</th>
-			</tr>
-			<tr>
-				<th style='background-color: lightgrey;'>Ulica</th>
-				<th style='background-color: lightgrey;'>Mieszkanie</th>
-				<th style='background-color: lightgrey;'>Kod pocztowy</th>
-				<th style='background-color: lightgrey;'>Miasto</th>
-			</tr>
+			<thead>
+				<tr>
+					<th rowspan='2' style='background-color: lightgrey; font-size: 1.4em;'>ID</th>
+					<th style='background-color: lightgrey;'>Imię</th>
+					<th style='background-color: lightgrey;'>Nazwisko</th>
+					<th style='background-color: lightgrey;'>Telefon</th>
+					<th style='background-color: lightgrey;'>E-mail</th>
+					<th rowspan='2' style='background-color: lightgrey;'>E</th>
+					<th rowspan='2' style='background-color: lightgrey;'>U</th>
+				</tr>
+				<tr>
+					<th style='background-color: lightgrey;'>Ulica</th>
+					<th style='background-color: lightgrey;'>Mieszkanie</th>
+					<th style='background-color: lightgrey;'>Kod pocztowy</th>
+					<th style='background-color: lightgrey;'>Miasto</th>
+				</tr>
+			</thead>
 
 			<?php
 				while($row = oci_fetch_array($klienci)) {
-					echo"<tr>";
-					echo"<td rowspan='2' style='font-size: 1.4em;'>".$row['ID']."</td>";
-					echo"<td>".$row['IMIE']."</td>";
-					echo"<td>".$row['NAZWISKO']."</td>";
-					echo"<td>".$row['TELEFON']."</td>";
-					echo"<td>".$row['EMAIL']."</td>";
-					echo"</tr>";
-					echo"<tr>";
-					echo"<td style='border-bottom: solid 1px lightgrey;'>".$row['ULICA']."</td>";
-					echo"<td style='border-bottom: solid 1px lightgrey;'>".$row['MIESZKANIE']."</td>";
-					echo"<td style='border-bottom: solid 1px lightgrey;'>".$row['KOD_POCZTOWY']."</td>";
-					echo"<td style='border-bottom: solid 1px lightgrey; border-right: solid 1px lightgrey;'>".$row['MIASTO']."</td>";
-					echo"</tr>";
+					echo"<tbody id='".$row['ID']."'>";
+						echo"<tr>";
+							echo"<td rowspan='2' style='font-size: 1.4em;'>".$row['ID']."</td>";
+							echo"<td>".$row['IMIE']."</td>";
+							echo"<td>".$row['NAZWISKO']."</td>";
+							echo"<td>".$row['TELEFON']."</td>";
+							echo"<td>".$row['EMAIL']."</td>";
+							echo"<td rowspan='2'>E</td>";
+							echo"<td rowspan='2'>U</td>";
+						echo"</tr>";
+						echo"<tr>";
+							echo"<td style='border-bottom: solid 1px lightgrey;'>".$row['ULICA']."</td>";
+							echo"<td style='border-bottom: solid 1px lightgrey;'>".$row['MIESZKANIE']."</td>";
+							echo"<td style='border-bottom: solid 1px lightgrey;'>".$row['KOD_POCZTOWY']."</td>";
+							echo"<td style='border-bottom: solid 1px lightgrey; border-right: solid 1px lightgrey;'>".$row['MIASTO']."</td>";
+						echo"</tr>";
+					echo"</tbody>";
 				}
 			?>
 
