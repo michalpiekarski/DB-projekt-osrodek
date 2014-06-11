7ad502e3289db46deb214a9a9c05b198a51f7245
Stolarz <tomek.solik@outlook.com>
2014-06-11 18:38:33 +0200

    Nowy plik edit 2. Nie chciało mi się kombinować i wstawianie do bazy odbywać się będzie w tym właśnie pliku. Poprawki#2

--- admin/edit2.php
+++ admin/edit2.php
@@ -19,23 +19,7 @@
             clear: both;
         }
     </style>
-        <script type="text/javascript">
-        function Table(){
-        var Row = document.getElementById("remove");
-        var Cells = Row.getElementsByTagName("td");
-       
-         var id = Cells[0].innerText;
-         var osrodek = Cells[1].innerText;
-         var typ = Cells[2].innerText;
-         var budynek = Cells[3].innerText;
-         var numer = Cells[4].innerText;
         
-        window.location.href="edit2.php?id="+ id +"&osrodek=" + osrodek;
-
-
-        alert(id);
-        }
-    </script>
 </head>
 <body>
 
@@ -54,7 +38,7 @@
         
 
     ?>
-<form action="edit2.php">
+
     <table class='basic-grey' style='border: none; padding: 0; text-align: center;' cellpadding='5em'>
         <tr>
             <th colspan = '5'style='background-color: lightgrey;'>Edycja</th>
@@ -68,24 +52,22 @@
         </tr>
         <tr id="remove">
            
-            <td><div contenteditable="true"><?php echo $id;?></div></td>
-            <td><div contenteditable="true"><?php echo $osrodek;?></div></td>
-            <td><div contenteditable="true"><?php echo $typ;?></div></td>
-            <td><div contenteditable="true"><?php echo $budynek;?></div></td>
-            <td><div contenteditable="true"><?php echo $numer;?></div></td>
+            <td><?php echo $id;?></div></td>
+            <td><?php echo $osrodek;?></div></td>
+            <td><?php echo $typ;?></div></td>
+            <td><?php echo $budynek;?></div></td>
+            <td><?php echo $numer;?></div></td>
             
         </tr>
         <tr>
             <th style='background-color: lightgrey;'><a href="obiekty.php">Wróć</a></th>
-            <th style='background-color: lightgrey;'><input type='submit' name='button' onclick="Table();" value='Zapisz'></th>
+           
         </tr>
     </table>
 
-</form>
 
 
 
-?>
 
 </body>
 </html>
