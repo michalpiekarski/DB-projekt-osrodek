6d8879e0bd4ffca183592cebbeb66811545e6c94
Michał Piekarski <michal.piekarski@icloud.com>
2014-06-11 20:50:44 +0200

    Dodana poprawna stylizacja grafik na głównych stronach części głównej i administracyjnej. Zalecane dodanie stanów :hover.

--- admin/index.php
+++ admin/index.php
@@ -4,18 +4,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <link rel="stylesheet" type="text/css" href="../css/menu.css" />
     <link rel="stylesheet" type="text/css" href="../css/form.css" />
-    <style type="text/css">
-        .imagelink {
-            width: 120px;
-            height: 120px;
-            float: left;
-            margin-left: 20px;
-            margin-bottom: 10px;
-        }
-        .clear {
-            clear: both;
-        }
-    </style>
+    <link rel="stylesheet" type="text/css" href="../css/grid.css" />
 </head>
 <body>
 
@@ -23,19 +12,21 @@
         include('nav.php');
     ?>
 
-    <div class="basic-grey" style="margin-top: 80px; width: 720px;">
+    <div class="basic-grey" style="margin-top: 80px; width: 720px; text-align: center;">
         <a href="osrodki.php">
             <img src="images/osrodki.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
         </a>
-        <a href="osrodek.php">
-            <img src="images/dodaj_osrodek.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
-        </a>
-        <div class="clear"></div>
         <a href="klienci.php">
             <img src="images/klienci.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
         </a>
         <a href="pracownicy.php">
             <img src="images/pracownicy.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
+        </a>
+        <a href="obiekty.php">
+            <img src="images/obiekty.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
+        </a>
+        <a href="osrodek.php">
+            <img src="images/dodaj_osrodek.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
         </a>
         <a href="stanowisko.php">
             <img src="images/dodaj_stanowisko.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
@@ -43,10 +34,7 @@
         <a href="pracownik.php">
             <img src="images/dodaj_pracownika.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
         </a>
-        <div class="clear"></div>
-        <a href="obiekty.php">
-            <img src="images/obiekty.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
-        </a>
+        <img src="images/tm.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
         <a href="typ_obiektu.php">
             <img src="images/dodaj_typ_obiektu.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
         </a>
@@ -59,7 +47,6 @@
         <a href="inny_obiekt.php">
             <img src="images/dodaj_inny_obiekt.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
         </a>
-        <div class="clear"></div>
         <a href="typy_zamowien.php">
             <img src="images/typy_zamowien.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
         </a>
@@ -72,7 +59,6 @@
         <a href="wypozyczenie.php">
             <img src="images/dodaj_typ_wypozyczenia.PNG" alt="Aktualne Rezerwacje" class="imagelink" />
         </a>
-        <div class="clear"></div>
     </div>
 </body>
 </html>
