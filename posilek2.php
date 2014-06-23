3e3b1d415489349ae954d677cfda2409e24d4bdd
Stolarz <tomek.solik@outlook.com>
2014-06-23 17:27:33 +0200

    [POSIŁKI]Dodano tworzenie rachunku w wypadku gdy wcześniejszy rachunek został opłacony i zamknięty.

--- posilek2.php
+++ posilek2.php
@@ -37,6 +37,11 @@
 
 			$id_klienta = $_POST['ID'];
 
+			$rachunek_stan = oci_parse($con, "Select ZAPLACONY from RACHUNKI where klient = $id");
+			oci_execute($rachunek_stan);
+			$rachunek_stan2 = oci_fetch_array($rachunek_stan);
+			$stan_rachunku = $rachunek_stan2['ZAPLACONY'];
+			
 			$cena_posilki = oci_parse($con, "Select cena from posilki where nazwa = '$posilek_nazwa'");
 			oci_execute($cena_posilki);
 			$cena_posilki2 = oci_fetch_array($cena_posilki);
@@ -46,30 +51,46 @@
 			oci_execute($rachunek_kwota);
 			$rachunek_kwota2 = oci_fetch_array($rachunek_kwota);
 			$rachunek_kwota3 = $rachunek_kwota2['KWOTA'];
-
 			$nowa_kwota = ($posilki_cena * $posilek_ilosc) + $rachunek_kwota3;
-			$dodaj = oci_parse($con, "UPDATE RACHUNKI SET KWOTA = $nowa_kwota where KLIENT = '$id_klienta'");
-			oci_execute($dodaj);
+			
+			if($stan_rachunku = 0)
+			{
+	
+				$dodaj = oci_parse($con, "UPDATE RACHUNKI SET KWOTA = $nowa_kwota where KLIENT = '$id_klienta'");
+				oci_execute($dodaj);
 
+			    $id_rachunku = oci_parse($con, "Select ID from rachunki where KLIENT = '$id_klienta' and ZAPLACONY = 0");
+			    oci_execute($id_rachunku);
+			    $id_rachunku2 = oci_fetch_array($id_rachunku);
+			    $id_rachunek = $id_rachunku2['ID'];
 
+			    $dodaj_usluge = oci_parse($con, "Insert into ZAMOWIENIA_POSILKOW (RACHUNEK,ILOSC,TYP,DATA) VALUES ('$id_rachunek','$posilek_ilosc','$posilek_nazwa','$posilek_data')");
+			    oci_execute($dodaj_usluge);
+				oci_close($con);
+			} 
+			else
+			{
+			
+				$sql_klient = oci_parse($con, "Insert into RACHUNKI (KLIENT, KWOTA) VALUES ('$id_klienta', '$nowa_kwota')");
+				oci_execute($sql_klient);
+				oci_close($con);
 
+				$id_rachunku = oci_parse($con, "Select ID from rachunki where KLIENT = '$id_klienta' AND ZAPLACONY = 0");
+			    oci_execute($id_rachunku);
+			    $id_rachunku2 = oci_fetch_array($id_rachunku);
+			    $id_rachunek = $id_rachunku2['ID'];
 
-		    $id_rachunku = oci_parse($con, "Select ID from rachunki where KLIENT = '$id_klienta'");
-		    oci_execute($id_rachunku);
-		    $id_rachunku2 = oci_fetch_array($id_rachunku);
-		    $id_rachunek = $id_rachunku2['ID'];
+			    $dodaj_usluge = oci_parse($con, "Insert into ZAMOWIENIA_POSILKOW (RACHUNEK,ILOSC,TYP,DATA) VALUES ('$id_rachunek','$posilek_ilosc','$posilek_nazwa','$posilek_data')");
+			    oci_execute($dodaj_usluge);
+				oci_close($con);
 
-		    $dodaj_usluge = oci_parse($con, "Insert into ZAMOWIENIA_POSILKOW (RACHUNEK,ILOSC,TYP,DATA) VALUES ('$id_rachunek','$posilek_ilosc','$posilek_nazwa','$posilek_data')");
-		    oci_execute($dodaj_usluge);
-			oci_close($con);
+			}
 		?>
-
 		<label>Posiłek zamówiony</label>
 		<label>
 			<span>&nbsp;</span>
 			<a href="rachunki_otwarte.php" class='button' >Przejdź do Rachunku Klienta</a>
 		</label>
 	</form>
-
 </body>
 </html>
